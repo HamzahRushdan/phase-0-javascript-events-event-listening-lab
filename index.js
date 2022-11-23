826acbab7719449127239c00a8c1478ca1489c6b
Hamzah Rushdan <hamzahrushdan79@gmail.com>
2022-11-23 09:52:56 -0500

    commit tree 749777a566886574751c9ca593fb27eaa7ce04f8

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-function addingEventListener() {
+//Element.addEventListener("click", myfunction);
+
+//function clickAlert() {
+ //   alert('click me');
+//}
+//const input = document.getElementById('button');
+
+//function clickAlert() {
+  //alert('I was clicked!');
+//}
+//clickAlert;
+//input.addEventListener('click', clickAlert);
+
+function addingEventListener (){
+    const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+addingEventListener();
